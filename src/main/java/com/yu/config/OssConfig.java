dd3306347594e3fa33674a347777bce5a1277288
cocochimp <2427886409@qq.com>
2023-10-10 15:29:24 +0800

    修改info

--- src/main/java/com/yu/config/OssConfig.java
+++ src/main/java/com/yu/config/OssConfig.java
@@ -13,9 +13,9 @@ public class OssConfig {
 
     @Bean
     public OSS ossClient(){
-        String endpoint = "oss-cn-shenzhen.aliyuncs.com";
-        String accessKeyId = "LTAI5t7rtwQwHL7S1SarTTma";
-        String accessKeySecret = "AN3Q00XDABlCfpiU2WMovmWYDAzuUy";
+        String endpoint = "xxx";
+        String accessKeyId = "xxx";
+        String accessKeySecret = "xxx";
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
         return ossClient;
     }
